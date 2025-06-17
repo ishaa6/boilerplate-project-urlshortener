994653e4f492356bd65e648dd0019ac433c159c5
ishaa6 <127003246@sastra.ac.in>
2025-06-17 23:25:28 +0530

    Accessing data using short url

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 require('dotenv').config();
 const express = require('express');
-const cors = require('cors');
 const app = express();
-
-// Basic Configuration
-const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const dns = require('dns');
 
 app.use(cors());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.json());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -14,11 +15,42 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+const urls = [];
+let idCounter = 1;
+
+app.post('/api/shorturl', (req, res) => {
+  const orgUrl = req.body.url;
+  try{
+    const urlObj = new URL(orgUrl);
+    if(urlObj.protocol !== 'http:' && urlObj.protocol !== 'https:') {
+      return res.json({ error: 'invalid url' });
+    }
+
+    dns.lookup(urlObj.hostname, (err) => {
+      if(err){
+        return res.json({ error: 'invalid url' });
+      }
+        const shortUrl = idCounter++; 
+        urls.push({original_url: orgUrl, short_url: shortUrl});
+        return res.json({ original_url: orgUrl, short_url: shortUrl });
+      });
+    }
+  catch (error) {
+    return res.json({ error: 'invalid url' });
+  }
 });
 
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
+app.get('/api/shortUrl/:shortUrl', (req, res) => {
+  const id = parseInt(req.params.shortUrl);
+  const urlEntry = urls.find((u) => u.short_url === id);
+  if(urlEntry) {
+    return res.redirect(urlEntry.original_url);
+  } else {   
+    return res.json({ error: 'No short URL found for the given input' });
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function() {
+  console.log(`Listening on port ${PORT}`);
 });
